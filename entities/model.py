afef28f0eb78bfcbca281207cf28e0b4436d93a4
Amir-Montazeri <m3amir88@yahoo.com>
2024-05-12 13:56:47 +0430

    clearifying modules

--- entities/model.py
+++ entities/model.py
@@ -1,54 +1,11 @@
 import pandas as pd
 import numpy as np
-# from modules import FrequencyEncoder
+from sklearn.preprocessing import OrdinalEncoder
+from modules import FrequencyEncoder
 from pydantic import BaseModel
 import matplotlib.pyplot as plt
 # import seaborn as sns
 # from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-
-def calc_frequency(df, column_name: str):
-  enc_column = (df.groupby(column_name).size()) / len(df)
-  return enc_column
-
-class FrequencyEncoder:
-  def __init__(self) -> None:
-    self.frequency_list = {}
-
-  def fit(self, df):
-    print('-----------------------------------------------')
-    print('fit: ', df)
-    print('-----------------------------------------------')
-
-  def fit_transform(self, df, df2):
-    print('-----------------------------------------------')
-    df_copy = df.copy()
-    for column in df_copy.columns:
-      self.frequency_list.update({column: calc_frequency(df, column)})
-      df_copy[column] = df[column].apply(lambda x: self.frequency_list[column][x])
-    df_copy.columns = df.columns
-    print('fit_transform: ', df_copy.columns)
-    print('-----------------------------------------------')
-    return df_copy
-  
-  def transform(self, df):
-    print('-----------------------------------------------')
-    print('transformer: ', df)
-    print('-----------------------------------------------')
-    df_copy = df.copy()
-    for column in df_copy.columns:
-      df_copy[column] = df[column].apply(lambda x: self.frequency_list[column][x])
-    df_copy.columns = df.columns
-    return df_copy
-
-  def get_params(self, deep = True):
-    return self.frequency_list
-  
-  def get_feature_names_out(self, col_list):
-    calced_col_list = self.frequency_list.keys()
-    return calced_col_list
-
-from sklearn.preprocessing import OrdinalEncoder
-# ---------------------------------------------------------------------------------------------------------------
 
 df = pd.read_csv('salaries.csv')
 # del
--- entities/modules.py
+++ entities/modules.py
@@ -2,31 +2,6 @@ def calc_frequency(df, column_name: str):
   enc_column = (df.groupby(column_name).size()) / len(df)
   return enc_column
 
-class FrequencyEncoder:
-  # rewrite the function as a class
-  def __init__(self) -> None:
-    self.frequency_list = {}
-
-  def fit(self, df):
-    pass
-
-  def fit_transform(self, df, column_list):
-    # df_copy = df.copy()
-    print(df)
-    return df
-  #   for column in column_list:
-  #     self.frequency_list.update({column: calc_frequency(df, column)})
-  #     df_copy[column] = df[column].apply(lambda x: self.frequency_list[column][x])
-  #   return df_copy
-  
-  # def transform(self, df, column_list):
-  #   df_copy = df.copy()
-  #   for column in column_list:
-  #     df_copy[column] = df[column].apply(lambda x: self.frequency_list[column][x])
-
-  def get_params(self, deep = True):
-    return self.frequency_list
-
 from time import time
 from threading import Event, Thread 
 class setInterval:
@@ -45,3 +20,40 @@ class setInterval:
 
   def cancel(self):
     self.stopEvent.set()
+
+class FrequencyEncoder:
+  def __init__(self) -> None:
+    self.frequency_list = {}
+
+  def fit(self, df):
+    print('-----------------------------------------------')
+    print('fit: ', df)
+    print('-----------------------------------------------')
+
+  def fit_transform(self, df, df2):
+    print('-----------------------------------------------')
+    df_copy = df.copy()
+    for column in df_copy.columns:
+      self.frequency_list.update({column: calc_frequency(df, column)})
+      df_copy[column] = df[column].apply(lambda x: self.frequency_list[column][x])
+    df_copy.columns = df.columns
+    print('fit_transform: ', df_copy.columns)
+    print('-----------------------------------------------')
+    return df_copy
+  
+  def transform(self, df):
+    print('-----------------------------------------------')
+    print('transformer: ', df)
+    print('-----------------------------------------------')
+    df_copy = df.copy()
+    for column in df_copy.columns:
+      df_copy[column] = df[column].apply(lambda x: self.frequency_list[column][x])
+    df_copy.columns = df.columns
+    return df_copy
+
+  def get_params(self, deep = True):
+    return self.frequency_list
+  
+  def get_feature_names_out(self, col_list):
+    calced_col_list = self.frequency_list.keys()
+    return calced_col_list
